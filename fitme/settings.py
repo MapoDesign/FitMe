d0db676d9d890a3f3bd60a63a687fcc7e0efaf75
MapoDesign <elisaresmini@gmail.com>
2022-10-26 11:15:30 +0200

    debug

--- fitme/settings.py
+++ fitme/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4yt#3w-by+!b)dis48q58x^jx%obg8i6w2)qj=e=i#%um@vh&@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '*']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
